2f29d572a60df344b6ebd2575b2b3c8c676282a3
Sam <LEE250716@spengergasse.at>
2025-09-18 12:57:16 +0200

    Initial sharing of project

---
+++ Computer.java
@@ -0,0 +1,32 @@
+public class Computer
+{
+    private String  hersteller;
+    private boolean laptop;
+    private int     preis;
+    
+    public String getHersteller()
+    {
+        return hersteller;
+    }
+    public boolean getLaptop()
+    {
+        return laptop;
+    }
+    public int getPreis()
+    {
+        return preis;
+    }
+    
+    public void setHersteller(String neuHersteller)
+    {
+        hersteller = neuHersteller;
+    }
+    public void setLaptop(boolean neuLaptop)
+    {
+        laptop = neuLaptop;
+    }
+    public void setPreis(int neuPreis)
+    {
+        preis = neuPreis;
+    }
+}
